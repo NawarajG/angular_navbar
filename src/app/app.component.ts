c7d76a85f033086281938f33332a5a80d72a00e0
NawarajG <gnawaraj874@gmail.com>
2025-01-17 14:25:55 +0545

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [],
+  template: `
+    <nav>
+      <a [routerLink]="'/'">Home</a>
+      <a [routerLink]="'/about'">About</a>
+      <a [routerLink]="'/contact'">Contact</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-tailwind-nav';
-}
+export class AppComponent {}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Includes all Angular component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
